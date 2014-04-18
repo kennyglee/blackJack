3e57248d7404eda3b2c0ec5149c666ba7d7d84a5
kenny Lee <kenny.lee.93@gmail.com>
2014-04-17 20:34:25 -0700

    first commit

---
+++ src/Table.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+
+/**
+ * @author Kenny
+ *
+ */
+
+public class Table {
+	
+	// final constants
+	private final int maxSeat = 6;  /** Maximum of 6 people per table **/
+	
+	//instance variables
+	private int minBuyIn;			/** Minimum Buy In Allowed at the table **/
+	private int maxBuyIn;			/** Maximum Buy In Allowed at the table **/
+	private int	numOfDecks;			/** Number of decks ( single , double, six deck) **/
+	
+	
+	private ArrayList<Player> listOfPlayers;
+	
+	public Table (int numOfDecks, int minBuyIn, int maxBuyIn) {
+		this.numOfDecks = numOfDecks;
+		this.minBuyIn = minBuyIn;
+		this.maxBuyIn = maxBuyIn;
+		
+		switch (this.numOfDecks) {
+		
+			case 1:
+				ArrayList<Integer> deck = new ArrayList<Integer>(52);
+				System.out.println(deck.size());			
+		}
+	}
+	
+	public int getMinBuyIn() {
+		return this.minBuyIn;
+	}
+	
+	public int getMaxBuyIn() {
+		return this.maxBuyIn;
+	}
+	
+	public int getNumOfDecks() {
+		return this.numOfDecks;
+	}
+	
+	public static ArrayList<Integer> fillInDeck(ArrayList<Integer> deck){
+		
+		return deck;
+	}
+	
+	public static ArrayList<Integer> shuffle(ArrayList<Integer> deck){
+		
+		return deck;
+	
+	}
+	
+	
+}
